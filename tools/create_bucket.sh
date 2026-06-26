5ec3a6f33d805e86f0df3e095f1a9c625854933d
Guillaume VIEL <guillaume.viel@gmail.com>
2026-06-26 16:19:35 +0200

    fix: renommer le bucket S3 cscd → cdsd dans create_bucket.sh

--- tools/create_bucket.sh
+++ tools/create_bucket.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Création du bucket S3 'cscd-kayak-datalake' dans eu-west-3
+# Création du bucket S3 'cdsd-kayak-datalake' dans eu-west-3
 # Prérequis : credentials AWS valides dans .env ou dans ~/.aws/credentials
 # Usage : bash tools/create_bucket.sh  (depuis la racine du projet ou n'importe où)
 set -e
@@ -14,6 +14,6 @@ AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
 AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
 AWS_DEFAULT_REGION=${AWS_REGION:-eu-west-3} \
 aws s3api create-bucket \
-    --bucket "${S3_BUCKET:-cscd-kayak-datalake}" \
+    --bucket "${S3_BUCKET:-cdsd-kayak-datalake}" \
     --region "${AWS_REGION:-eu-west-3}" \
     --create-bucket-configuration LocationConstraint="${AWS_REGION:-eu-west-3}"
